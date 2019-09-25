4028ef27a682e47bb9e33beb31eef87518685ecb
tangweikun <819573105@qq.com>
2019-09-25 15:07:30 +0800

    :sparkles: day-47

---
+++ day-47/index.js
@@ -0,0 +1,16 @@
+function convert(list) {
+	const res = []
+	const map = list.reduce((res, v) => (res[v.id] = v, res), {})
+	for (const item of list) {
+		if (item.parentId === 0) {
+			res.push(item)
+			continue
+		}
+		if (item.parentId in map) {
+			const parent = map[item.parentId]
+			parent.children = parent.children || []
+			parent.children.push(item)
+		}
+	}
+	return res
+}
